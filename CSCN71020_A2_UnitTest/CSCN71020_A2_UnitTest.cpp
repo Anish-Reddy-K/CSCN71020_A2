632551430de440c1bb3305279cadd458744e07f6
Anish-Reddy-K <akanamathareddy5037@conestogac.on.ca>
2023-03-10 21:39:16 -0500

    Changes to getPerimeter, setLength and setWidth

--- CSCN71020_A2_UnitTest/CSCN71020_A2_UnitTest.cpp
+++ CSCN71020_A2_UnitTest/CSCN71020_A2_UnitTest.cpp
@@ -9,7 +9,7 @@ namespace CSCN71020A2UnitTest
 	TEST_CLASS(CSCN71020A2UnitTest)
 	{
 	public:
-		
+
 		// Test Case: getPerimeter
 		TEST_METHOD(getPerimeter_test)
 		{
@@ -38,7 +38,7 @@ namespace CSCN71020A2UnitTest
 			int input = 0;
 			int expected_length = 1;
 			setLength(input, &expected_length);
-			Assert::AreEqual(input, expected_length);
+			Assert::AreNotEqual(input, expected_length);
 		}
 
 		// Test Case 2: setLength - 88
@@ -56,17 +56,17 @@ namespace CSCN71020A2UnitTest
 			int input = 100;
 			int expected_length = 99;
 			setLength(input, &expected_length);
-			Assert::AreEqual(input, expected_length);
+			Assert::AreNotEqual(input, expected_length);
 		}
 
-		
+
 		// Test Case 1: setWidth - 0
 		TEST_METHOD(setWidth_test_1)
 		{
 			int input = 0;
 			int expected_width = 1;
 			setWidth(input, &expected_width);
-			Assert::AreEqual(input, expected_width);
+			Assert::AreNotEqual(input, expected_width);
 		}
 
 		// Test Case 2: setWidth - 75
@@ -89,3 +89,5 @@ namespace CSCN71020A2UnitTest
 
 	};
 }
+
+
